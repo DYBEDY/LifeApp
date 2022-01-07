19705dad76d69423404f6919d4dc590ce3eeb8c7
Elisei Mishchenko <esm.mail@icloud.com>
2022-01-07 22:36:15 +0300

    Working with the developer model - creating a developer structure and extensions for this structure in which data is stored

--- LifeTest/Models/Developer.swift
+++ LifeTest/Models/Developer.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+struct Developer {
+    let fullName: String
+    let work: String
+    let dateOfBirth: String
+    
+    var title: String {
+        "\(fullName) - \(work)"
+    }
+}
+
+extension Developer {
+    static func getDeveloperList() -> [Developer] {
+        [
+            Developer(fullName: "Diana Ovechkina",
+                      work: "Developer",
+                      dateOfBirth: "07.03.2001"),
+            Developer(fullName: "Anna Mel",
+                      work: "Developer",
+                      dateOfBirth: "05.09.1992"),
+            Developer(fullName: "Roma",
+                      work: "Developer",
+                      dateOfBirth: "10.05.1994"),
+            Developer(fullName: "Elisei Mishchenko",
+                      work: "Developer",
+                      dateOfBirth: "10.10.2007")
+        ]
+    }
+}

3fa59036f9afc11725dabf3861cb463acf92c1f6
Ovechkina Diana <ovechkinadiana@yandex.ru>
2022-01-07 23:15:39 +0700

    check

--- LifeTest/ViewControllers/LivedDaysViewController.swift
+++ LifeTest/ViewControllers/LivedDaysViewController.swift
@@ -9,21 +9,44 @@ import UIKit
 
 class LivedDaysViewController: UIViewController {
 
+    @IBOutlet var todayLabel: UILabel!
+    @IBOutlet var passedDaysLabel: UILabel!
+    
+    var birthDate = "07.03.2001"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        todayLabel.text = "Сегодня\n \(getCurrentDate())"
+        
+        passedDaysLabel.layer.cornerRadius = 15
+        passedDaysLabel.layer.masksToBounds = true
+        passedDaysLabel.text = "\n Ты прожил уже \n \(dateInterval(beginDate: birthDate, endDate: getCurrentDate()))\n"
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - private methods
+    
+   private func getCurrentDate() -> String {
+        let currentDate = DateFormatter()
+        currentDate.dateFormat = "dd.MM.yyyy"
+        
+        return currentDate.string(from: Date())
     }
-    */
-
+    
+    
+   private func dateInterval(beginDate: String, endDate: String) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.yyyy"
+        
+        guard let begin = dateFormatter.date(from: beginDate) else { return "" }
+        guard let end = dateFormatter.date(from: endDate) else { return "" }
+        
+        let dateInterval = Calendar.current.dateComponents([.day], from: begin, to: end)
+        
+        guard let days = dateInterval.day else {return ""}
+        
+        let resultDateInterval = " \(days) дней"
+            
+        return resultDateInterval
+    }
 }

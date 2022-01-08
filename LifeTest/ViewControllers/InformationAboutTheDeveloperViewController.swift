78c90c9c154512c9fadafb020d8644f2089fba0f
Elisei Mishchenko <esm.mail@icloud.com>
2022-01-08 19:01:07 +0300

    Transfer via delegate from tabBarViewController to the InformationAboutTheDeveloperViewController

--- LifeTest/ViewControllers/InformationAboutTheDeveloperViewController.swift
+++ LifeTest/ViewControllers/InformationAboutTheDeveloperViewController.swift
@@ -17,36 +17,16 @@ class InformationAboutTheDeveloperViewController: UIViewController {
     
     //MARK: - Public properties
     var developer: Developer!
-
+    var delegate: DaysViewControllerDelegate!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         developerImage.layer.cornerRadius = 20
         developerImage.image = UIImage(named: developer.title)
         dateOfBirthLabel.text = developer.dateOfBirth
-        youLivedLabel.text = dateInterval(beginDate: developer.dateOfBirth, endDate: getCurrentDate())
-    }
-    
-    //MARK: - Private methods
-    private func getCurrentDate() -> String {
-        let currentDate = DateFormatter()
-        currentDate.dateFormat = "dd.MM.yyyy"
-        
-        return currentDate.string(from: Date())
-    }
-    
-    private func dateInterval(beginDate: String, endDate: String) -> String {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "dd.MM.yyyy"
-        
-        guard let begin = dateFormatter.date(from: beginDate) else { return "" }
-        guard let end = dateFormatter.date(from: endDate) else { return "" }
-        
-        let dateInterval = Calendar.current.dateComponents([.day], from: begin, to: end)
-        
-        guard let days = dateInterval.day else {return ""}
-        
-        let resultDateInterval = " \(days) days"
-        
-        return resultDateInterval
+        youLivedLabel.text = delegate?.dateInterval(
+            beginDate: developer.dateOfBirth,
+            endDate: delegate?.getCurrentDate() ?? ""
+        )
     }
 }

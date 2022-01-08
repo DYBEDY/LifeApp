6f59231ee744c391dc6c68b3f90b52ee652c2267
DYBEDY <rd.iosdeveloper@gmail.com>
2022-01-08 20:07:50 +0300

    with new background

--- LifeTest/ViewControllers/StartViewController.swift
+++ LifeTest/ViewControllers/StartViewController.swift
@@ -10,20 +10,42 @@ import AVFoundation
 
 class StartViewController: UIViewController {
     
-override func viewDidLoad() {
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var presentationLabel: UILabel!
+    @IBOutlet var videoLayer: UIView!
+    @IBOutlet var startButton: UIButton!
+    
+    
+    
+    override func viewDidLoad() {
         super.viewDidLoad()
-        installBackgrounColor()
-         playVideo()
+//        installBackgrounColor()
+        playVideo()
+        
     }
 
-    private func installBackgrounColor() {
-        let gradientLayer = CAGradientLayer()
-        gradientLayer.frame = view.bounds
-        gradientLayer.colors = [
-            UIColor.white.cgColor,
-            UIColor.systemIndigo.cgColor
-        ]
-        view.layer.insertSublayer(gradientLayer, at: 0)
+//    private func installBackgrounColor() {
+//        let gradientLayer = CAGradientLayer()
+//        gradientLayer.frame = view.bounds
+//        gradientLayer.colors = [
+//            UIColor.white.cgColor,
+//            UIColor.systemIndigo.cgColor
+//        ]
+//        view.layer.insertSublayer(gradientLayer, at: 0)
+//    }
+    
+    func playVideo() {
+        guard let path = Bundle.main.path(forResource: "testVideo", ofType: "mp4") else { return }
+        let player = AVPlayer(url: URL(fileURLWithPath: path))
+         let playerLayer = AVPlayerLayer(player: player)
+        playerLayer.frame = self.view.bounds
+        playerLayer.videoGravity = .resizeAspectFill
+        self.videoLayer.layer.addSublayer(playerLayer)
+        
+        player.play()
+        videoLayer.bringSubviewToFront(titleLabel)
+        videoLayer.bringSubviewToFront(presentationLabel)
+        videoLayer.bringSubviewToFront(startButton)
     }
   
 }

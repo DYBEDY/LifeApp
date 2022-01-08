b9ec7ab69b7b6a9257c3b6e00f23b6f6842464e4
Elisei Mishchenko <esm.mail@icloud.com>
2022-01-08 15:45:04 +0300

    Creating an IBOutlet in the AplicationDescriptionViewController file and passing text to this IBOutlet

--- LifeTest/ViewControllers/AplicationDescriptionViewController.swift
+++ LifeTest/ViewControllers/AplicationDescriptionViewController.swift
@@ -8,14 +8,15 @@
 import UIKit
 
 class AplicationDescriptionViewController: UIViewController {
+    
+    //MARK: - IBOutlets
+    @IBOutlet weak var informationLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        informationLabel.text = """
+In this application you can find out how long you have lived and how much you still want to live
+"""
         
     }
-    
-
-    
-
 }
